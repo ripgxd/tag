6b9a7a2ae284a593f86c82c458e242016af0427b
ripgxd <60903831+ripgxd@users.noreply.github.com>
2020-04-14 15:19:25 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,40 +5,42 @@ from config import api_id, api_hash
 client = TelegramClient('user', api_id, api_hash)
 client.start()
 
-admins = []
-global text
-
-
-
-# @client.on(events.NewMessage(pattern='!get'))
-# async def get_admins(event):
-#     admins.clear()
-#     async for user in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-#         print(f"{user.id} - {user.first_name} {user.last_name}")
-#         admins.append(user.id)
-#     print("done")
-    
 
 @client.on(events.NewMessage(pattern='!mn'))
 async def mention(event):
-    admins.clear()
-    async for user in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-        print(f"{user.id} - {user.first_name} {user.last_name}")
+    text = event.raw_text.replace("!mn ", "")
+    chatId = event.chat_id
+    counter = 0
+    admins = []
+
+    async for user in client.iter_participants(chatId, filter=ChannelParticipantsAdmins):
         admins.append(user.id)
-
+   
     if event.from_id in admins:
-        
-        text = text_buffer = event.raw_text.replace("!mn ", "")
-        async for user in client.iter_participants(event.chat_id):
-            
-            if user.id not in admins:
-            
-                await client.send_message(event.chat_id, f"[{text}](tg://user?id={user.id})")
 
-        await client.send_message(event.chat_id, ">КІНЧИВ<")
+        async for user in client.iter_participants(chatId, aggressive=True):
+            if user.id in admins:
+                continue
+            else:
+                counter += 1
+                mnText = getText(text, user.first_name, user.last_name)
+                client.send_message(chatId, f"[{mnText}](tg://user?id={user.id})")
+         
+        client.send_message(chatId, "Здається кінчив...")
 
-# def name(first, last):
-#     return first if last == None else f"{first} {last}"
+    print(f"{event.chat} - count: {counter}")
+    admins.clear()
+
+
+
+def name(first, last):
+    return first if last == None else f"{first} {last}"
+
+def getText(text, firstname, lastname):
+    if "name" in text:
+        return text.replace("name", name(firstname, lastname))
+    else:
+        return text
 
 with client:
     client.run_until_disconnected()
